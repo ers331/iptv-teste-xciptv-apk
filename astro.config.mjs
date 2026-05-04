e3689879a9edbd63897c4a2712938e48e65e7f09
Everson Ramiro da Silva <80833918+ers331@users.noreply.github.com>
2026-05-04 03:35:09 -0300

    sitemp2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://example.com'
+  site: process.env.SITE_URL || 'https://iptv-teste-xciptv-apk.vercel.app',
+  integrations: [sitemap()],
 });
